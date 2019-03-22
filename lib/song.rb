78f4e9248c252eb46387f38720029d6b37213fd6
Wendy Hamilton <wdhamilton117@gmail.com>
2019-03-22 14:17:53 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,14 +1,21 @@
 class Song
 
-@@all = []
+  attr_accessor :name, :artist, :genre
 
-def initialize
-  @@all << self
-end
+  @@all = []
 
-def self.all
-  @@all
-end
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+
 
 
 end
