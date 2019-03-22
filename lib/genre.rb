1cfb6b13326e71206e35528df77fc2836a0dfcf2
Wendy Hamilton <wdhamilton117@gmail.com>
2019-03-22 14:17:14 +0000

    Automatically backed up by Learn

--- lib/genre.rb
+++ lib/genre.rb
@@ -1,9 +1,11 @@
 class Genre
 
-attr_accessor :songs
+  attr_reader :name
+
   @@all = []
 
-  def initialize
+  def initialize(name)
+    @name = name
     @@all << self
   end
 
@@ -11,7 +13,28 @@ attr_accessor :songs
     @@all
   end
 
-  def songs
+  def new_song(name, artist)
+    ##new_song, that takes in an argument of a name and an artist and creates a new song. That song should know that it belongs to the genre.
+    Song.new(name, self, artist)
   end
+
+  def songs
+    ##songs, that iterates through all songs and finds the songs that belong to that genre
+    Songs.all.select do |song_instance|
+      song_instance.genre == self
+    end
+  end
+
+  def artists
+    #an instance method, #artists, that iterates over the genre's collection of songs and collects the artist that owns each song.
+      songs.map do |song_instance|
+        song_instance.artist
+      end
+  end
+
+  #this methis is in solution, but not lesson
+  # def add_songs(song)
+  #   self.songs << song
+  # end
 
 end
